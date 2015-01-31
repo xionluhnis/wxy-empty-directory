8a552fe31c088ee7406bca5f89b18658a84ca903
Alexandre Kaspar <xion.luhnis@gmail.com>
2015-01-31 05:37:27 +0100

    Empty directory plugin that aborts other 404 processing in case the requested file is a valid directory

---
+++ empty_directory.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once ROOT_DIR . '/files.php';
+
+class Empty_Directory {
+
+    public function after_404_load_content(&$file, &$content) {
+        if(Text::ends_with($file, '/index' . CONTENT_EXT)){
+            $dir = str_replace('/index' . CONTENT_EXT, '', $file);
+            if(is_dir($dir)){
+                $content = "Directory";
+            }
+            return true;
+        } elseif (Text::ends_with($file, CONTENT_EXT)) {
+            $dir = substr($file, 0, -strlen(CONTENT_EXT));
+            if(is_dir($dir)){
+                $content = "Directory";
+            }
+            return true;
+        }
+        return false;
+    }
+
+}
+
+?>
